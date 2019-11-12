f6aeea3104f1ad340489b49eb3f17cda9b8fd325
Andrew Pivovarov <pib2703@gmail.com>
2019-11-12 19:26:38 +0300

    Tests here

--- src/com/rakovets/course/challenge/classes/TimeTests.java
+++ src/com/rakovets/course/challenge/classes/TimeTests.java
@@ -1,4 +1,8 @@
 package com.rakovets.course.challenge.classes;
 
 public class TimeTests {
+    public static void main(String[] args) {
+        Time lessonTime = new Time(19, 8, 10);
+        lessonTime.display();
+    }
 }
