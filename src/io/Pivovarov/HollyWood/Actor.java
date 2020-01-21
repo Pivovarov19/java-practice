3a38c55188feda771a850ccb9bf1c3224554e493
Andrew Pivovarov <pib2703@gmail.com>
2020-01-21 19:24:08 +0300

    Solution: a part of project Hollywood

---
+++ src/io/Pivovarov/HollyWood/Actor.java
@@ -0,0 +1,22 @@
+package io.Pivovarov.HollyWood;
+
+import java.util.List;
+
+public class Actor {
+    private String firstName;
+    private String lastName;
+    private int fee;
+    private int age;
+
+    public Actor(String firstName, String lastName, int fee) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fee = fee;
+    }
+
+    public fire(Actor Actor){
+
+    }
+}
+
+
---
+++ src/io/Pivovarov/HollyWood/HollyWoodDemo.java
@@ -0,0 +1,14 @@
+package io.Pivovarov.HollyWood;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HollyWoodDemo {
+    public static void main(String[] args) {
+        List Actor = new ArrayList<Actor>;
+        Actor tony = new Actor("Tony", "Donny", 10_000);
+        Actor hell = new Actor("Hell", "bingdon", 9_999);
+        Actor blog = new Actor("Blog", "Klivenson", 100_000);
+        System.out.println();
+    }
+}
